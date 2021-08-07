b1e56d918c0fb53ee900ca4af6122764ae1dd674
Connor <connor.randall669@gmail.com>
2021-08-07 03:09:57 -0400

    test

---
+++ AnonymusIncomeComparison/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathAndComparisonsOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymus Income Comparison Program");
+            Console.ReadLine();
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate");
+            int hoursPerson1 = 15;
+            int hoursPerWeek1 = 40;
+            Console.WriteLine("How much per hour: " + hoursPerson1);
+            Console.WriteLine("How many hours per week: " + hoursPerWeek1);
+            Console.ReadLine();
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate");
+            int hoursPerson2 = 20;
+            int hoursPerWeek2 = 40;
+            Console.WriteLine("How much per hour: " + hoursPerson2);
+            Console.WriteLine("How many hours per week: " + hoursPerWeek2);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
