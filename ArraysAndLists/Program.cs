6b821658d3549c8c123d56bf7f4859c72f7fedbe
Connor <connor.randall669@gmail.com>
2021-08-17 14:26:01 -0400

    Arrays added

---
+++ ArraysAndLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+        
+       
+        
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+
+        byte[] byteArray = new byte[5000];
+        //ARRAYS START
+
+        //int[] numArray = new int[5]; //Declares the integer array and declares the length of the array
+        //numArray[0] = 5; 
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 }; //Same as above just condensed
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 }; //Same as above just even shorter
+
+        //numArray2[5] = 650;
+
+        //Console.WriteLine();
+        //Console.ReadLine();
+
+        //ARRAYS END
+    
+        }
+    }
+
---
+++ StringFunctions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Connor";
+            string quote = "The man said, \"Hello\", Connor. \n hello on a new line. \n \t hello on a tab. " ;
+            string fileName = @"C:\Users\Connor";
+
+            bool trueOrFalse = name.Contains("s");
+
+            Console.WriteLine(trueOrFalse);
+            Console.ReadLine();
+        }
+    }
+}
