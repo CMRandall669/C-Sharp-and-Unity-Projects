b5a0ea6c1b15e569e61219134353558999a12c00
Connor <connor.randall669@gmail.com>
2021-08-21 20:48:44 -0400

    Updates

---
+++ CallingMethodAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallingMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
