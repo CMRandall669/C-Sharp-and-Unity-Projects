228cbb0d59381ea8703d8545b91b21792840e234
Connor <connor.randall669@gmail.com>
2021-08-23 17:26:55 -0400

    update

--- CallingMethodAssignment/Program.cs
+++ CallingMethodAssignment/Program.cs
@@ -12,7 +12,7 @@ namespace CallingMethodAssignment
             Console.WriteLine("Start by inputing a number");
             input.userNumber();
 
-            Console.WriteLine(input.userNumber());
+            Console.WriteLine(input);
             
           
            
