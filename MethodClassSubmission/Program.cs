05d74ab8609ee3ef1d1f9c360f9aebbc906b0618
Connor <connor.randall669@gmail.com>
2021-08-25 15:29:18 -0400

    Upload

---
+++ MethodClassSubmission/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidClass empty = new voidClass();
+            
+
+            Console.WriteLine(empty.mathOperation());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassSubmission/voidClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    public class Test
+    {
+        //public int NumberOne { get; set; }
+        //public int NumberTwo { get; set; }
+
+        public void MathOperation()
+        {
+            Console.WriteLine("Please enter the first number: ");
+            int NumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You enter " + NumberOne + " as the first number.");
+
+            Console.WriteLine("Please enter the second number: ");
+            int NumberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You enter " + NumberTwo + " as the second number.");
+
+            int NumberThree = NumberOne + 250;
+            Console.WriteLine(NumberThree + " is the result of the first number plus 250 ");
+            Console.WriteLine("The second integer you provided was " + NumberTwo);
+            Console.ReadLine();
+            
+        }
+        
+
+    }
+}
