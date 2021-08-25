0d4acc7d672119df7b9ae1c60947a0ca820b5c74
Connor <connor.randall669@gmail.com>
2021-08-25 01:51:05 -0400

    MainMethodAssignment Upload

---
+++ MainMethodAssignment/Math.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        
+        public int integerInput { get; set; }
+        public decimal decimalInput { get; set; }
+        public string stringInput { get; set; }
+        public int loopCount = 4;
+        public string userResult()
+        {   
+            for (int i = 0; i < loopCount ; i++)
+            {
+                if (i < 1)
+                {
+                    Console.WriteLine("Please enter a whole number: ");
+                    int integerInput = Convert.ToInt32(Console.ReadLine());
+                    int endResult1 = integerInput * 10;
+                    Console.WriteLine("The number you entered times 10 equals:  " + endResult1);
+                    Console.ReadLine();
+                }
+
+                else if (i < 2)
+                {
+                    Console.WriteLine("Please enter a number with a decimal value: ");
+                    decimal decimalInput = Convert.ToDecimal(Console.ReadLine());
+                    decimal endResult2 = decimalInput + 13;
+                    Console.WriteLine("The decimal you entered plus 13 is:   " + endResult2);
+                    Console.ReadLine();
+                }
+
+
+
+                else if (i < 3)
+                {
+                    Console.WriteLine("Please enter a word and we will check if it beats our number");
+                    string stringInput = Convert.ToString(Console.ReadLine());
+                    if (stringInput.Length > 4)
+                    {
+                        Console.WriteLine("The word " + stringInput + " is longer than 4 letters");
+                    }
+
+                    if (stringInput.Length <= 4)
+                    {
+                        Console.WriteLine("The word " + stringInput + " is at least 4 letters long.");
+                    }
+                    Console.ReadLine();
+                    
+
+                }
+            }
+
+            return"";
+
+        }
+        
+
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            
+            Console.WriteLine(math.userResult());
+            Console.ReadLine();
+            
+
+
+
+
+
+            
+        }
+    }
+}
