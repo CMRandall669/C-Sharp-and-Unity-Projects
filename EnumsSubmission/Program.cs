72144be6ac55eaf20ef91a6ec1cf9532d1497f99
Connor <connor.randall669@gmail.com>
2021-09-01 03:26:19 -0400

    update

---
+++ EnumsSubmission/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnumsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day of the week");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
