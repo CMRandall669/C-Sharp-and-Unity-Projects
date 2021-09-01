7e0a9a19f76d3be83bacd19c5fe317932fd4e364
Connor <connor.randall669@gmail.com>
2021-08-31 22:17:58 -0400

    ParametersAssignment

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -10,17 +10,7 @@ namespace BlackJackGame
     {
         static void Main(string[] args)
         {
-            //List<Game> games = new List<Game>();
-            //TwentyOneGame game = new TwentyOneGame();
-            //games.Add(game);
-
-
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Connor";
-            game += player;
-            game -= player;
+            
             
             Deck deck = new Deck();
             deck.Shuffle(3);
@@ -32,16 +22,7 @@ namespace BlackJackGame
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
-
-        
-
-    //    public static Deck Shuffle(Deck deck, int times)
-    //    {
-    //        for (int i =0; i < times; i++)
-    //        {
-    //            deck = Shuffle(deck);
-    //        }
-    //        return deck;
+   
        }
     }
 
---
+++ ParametersAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>();
+            Employee<int> employee2 = new Employee<int>();
+            employee2.things = new List<int>();
+
+            employee.things.Add("Jeff");
+            employee.things.Add("Mark");
+            employee.things.Add("George");
+
+            employee2.things.Add(40);
+            employee2.things.Add(60);
+            employee2.things.Add(80);
+
+
+            for (int i = 0; i < employee.things.Count; i++)
+            {
+                Console.WriteLine(employee.things[i].ToString() + ", " + employee2.things[i].ToString());
+                
+            }
+            
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
