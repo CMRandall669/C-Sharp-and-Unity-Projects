4b3979752270c6e30b225a1a6d974975a4e6adaa
Connor <connor.randall669@gmail.com>
2021-09-02 00:59:34 -0400

    Upload

---
+++ LambdaAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAssignment
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string Name, int Id)
+        {
+            Name = Name;
+            Id = Id;
+        }
+    }
+}
---
+++ LambdaAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> employee = new List<Employee>();
+            List<string> joes = new List<string>();
+
+            employee.Add(new Employee { Name = "Andy Warhol", Id = 1 });
+            employee.Add(new Employee { Name = "Franklin Two", Id = 2 });
+            employee.Add(new Employee { Name = "Zao Shang", Id = 3 });
+            employee.Add(new Employee { Name = "Jackson Pollock", Id = 4 });
+            employee.Add(new Employee { Name = "Agent K", Id = 5 });
+            employee.Add(new Employee { Name = "Albus Dumbledore", Id = 6 });
+            employee.Add(new Employee { Name = "Alan Rickman", Id = 7 });
+            employee.Add(new Employee { Name = "Steve Buscemi", Id = 8 });
+            employee.Add(new Employee { Name = "Joe Dirt", Id = 9 });
+            employee.Add(new Employee { Name = "Joe Pistachio", Id = 10 });
+
+            foreach (var hire in employee)
+            {
+                string newHire = Convert.ToString(hire);
+                Console.WriteLine(newHire);
+            }
+            //Employee<string> employee2 = new Employee<string>();
+            //employee2.entry = new List<string>() { "Andy Warhol", "Franklin Two", "Zao Shang", "Jackson Pollock", "Agent K",
+            //"Albus Dumbledore", "Alan Rickman", "Steve Buscemi", "Joe Dirt",  "Joe Jonas"};
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
