54c127aa05ce2d25175e7656d11c975db870ee64
Connor Randall <83515675+CMRandall669@users.noreply.github.com>
2021-09-02 15:29:43 -0400

    update

---
+++ LambdaSubmission/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Employee = new List<string> { "Joe Dirt", "Joe Butch", "Jackson Pollock", "Andy Warhol",
+                "Billy Bob", "Missy Franklin", "Kelly Goober", "Ben Farmer", "Nickolus Flammel", "Albus Dumbledore" };
+
+            Console.WriteLine(Employee);
+            Console.ReadLine();
+
+
+        }
+    }
+}
