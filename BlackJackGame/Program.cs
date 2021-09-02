5ae6b5b035adceacf938e8716dbcc055133af065
Connor <connor.randall669@gmail.com>
2021-09-01 23:20:24 -0400

    update

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -11,16 +11,33 @@ namespace BlackJackGame
         static void Main(string[] args)
         {
 
+
             
 
             Deck deck = new Deck();
-            deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+
+
+            List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
+
+            //deck.Cards.
+            
+            //int counter = 0;
+            //foreach (Card card in deck.Cards)
+            //{
+            //    if (card.Face == Face.Ace)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
    
