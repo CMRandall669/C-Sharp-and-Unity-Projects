ecaca4299b3009da04c6e4320276ff188dc60973
Connor <connor.randall669@gmail.com>
2021-09-03 23:09:07 -0400

    update

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -11,36 +11,29 @@ namespace BlackJackGame
         static void Main(string[] args)
         {
 
-
-            
-
-            Deck deck = new Deck();
-
-
-
-            List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-
-            //deck.Cards.
-            
-            //int counter = 0;
-            //foreach (Card card in deck.Cards)
-            //{
-            //    if (card.Face == Face.Ace)
-            //    {
-            //        counter++;
-            //    }
-            //}
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to play a game of BlackJack right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now");
             Console.ReadLine();
         }
    
-       }
     }
+ }
 
