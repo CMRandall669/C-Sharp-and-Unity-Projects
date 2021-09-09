f4a61ac3c21e49585b83eab996d0f68d88efa8e9
Connor <connor.randall669@gmail.com>
2021-09-08 20:34:23 -0400

    update

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace DateTime
+namespace DateTimeSubmission
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             DateTime dt = new DateTime();
+
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+            Console.WriteLine("Please enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime newTime = currentTime.AddHours(userInput);
+            Console.WriteLine("The date and time in " + userInput + " hours will be " + newTime);
+            Console.ReadLine();
         }
     }
 }
