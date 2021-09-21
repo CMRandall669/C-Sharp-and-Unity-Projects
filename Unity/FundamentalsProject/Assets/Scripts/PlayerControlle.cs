601b8c0dabc82a3f527489b51c3d24a7b6c1aa52
Connor <connor.randall669@gmail.com>
2021-09-21 14:35:13 -0400

    Unity Upload

---
+++ Unity/FundamentalsProject/Assets/Scripts/PlayerControlle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerControlle : MonoBehaviour
+{
+    // private Animator anim;
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+  
+}
